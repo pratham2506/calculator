88ffcdd0c67606328b7bc6f0df5fff8c72faee30
Pratham Matkar <prathammatkar2506@gmail.com>
2023-12-14 11:16:53 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Wrapper from './components/Wrapper';
+import Screens from './components/Screens';
+import ButtonBox from './components/ButtonBox';
+import Button from './components/Button';
+import CalcProvider from './context/CalcContext';
+
+const btnValues=[
+  ["C","+-","%","/"],
+  [7,8,9,"x"],
+  [4,5,6,"-"],
+  [1,2,3,"+"],
+  [0,".","="],
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CalcProvider className='App'>
+      <Wrapper>
+      <Screens/>
+      <ButtonBox>
+        {btnValues.flat().map((btn,i)=>(
+          <Button value={btn} key={i}/>
+        ))}
+      </ButtonBox>
+      </Wrapper>
+    </CalcProvider>
   );
 }
 
